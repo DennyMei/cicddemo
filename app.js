c74ad88aeeaaec86a826fd0f812388345ddcd6d9
Denny Mei <yijunmei@gmail.com>
2026-02-27 13:55:55 +0800

    init

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const version = process.env.APP_VERSION || "dev";
+
+app.get('/', (req, res) => {
+  res.send(`Hello from ECS! Version: ${version}`);
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
